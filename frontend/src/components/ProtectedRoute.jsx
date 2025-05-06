164bec5ceb0cbb9f2dcd5c66ee7e847f01539bd5
Emmanuel Nsubuga <cemputus019@gmail.com>
2025-05-07 01:08:16 +0300

    code

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,14 +2,39 @@ import { Navigate } from "react-router-dom";
 import jwtDecode from "jwt-decode";
 import api from "../api";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import { useEffect, useState } from "react";
 
 const ProtectedRoute = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(null);
     
+
+
+    useEffect(() => {
+        auth().catch(() => setIsAuthorized(false));
+
+    }, []);
     const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        try{
+            const res = await api.post("api/token/refresh", {
+                 refresh: refreshToken, 
+                });
+
+                if (res.status === 200){
+                    const { access } = res.data;
+                    localStorage.setItem(ACCESS_TOKEN, access);
+                    setIsAuthenticated(true);
+                }else {
+                    setIsAuthenticated(false);
+                }
+
+        } catch (error){
+            console.log(error);
+            setIsAuthorized(false);
+        }
 
     
-}
+};
 
 const auth = async () => {
     const token = localStorage.getItem(ACCESS_TOKEN);
